38fbd45dddbff7311adebad8429cbbc6949542e2
Anessa Ortner <anessa.ortner@gmail.com>
2017-06-01 18:19:48 -0500

    fixed syntax issue

--- javascripts/controllers/EditUserCtrl.js
+++ javascripts/controllers/EditUserCtrl.js
@@ -17,13 +17,13 @@ app.controller("EditUserCtrl", function($location, $rootScope, $routeParams, $sc
   };
 
     getUserInfo();
+    console.log("button click object: ", $scope.userUpdate);
     
     $scope.updateUser = () => {
-    	
     	UserFactory.editEmail($scope.userUpdate.email);
-
 		console.log($rootScope.user.uid);
-		UserFactory.editUser($rootScope.user.uid, $scope.userUpdate).then(() => {
+		UserFactory.editUser($rootScope.user.uid, $scope.userUpdate).then((results) => {
+			console.log("update user function results: ", results);
 			console.log("working edit user?");
 		}).catch((error) => {
 			console.log("update user error: ", error);
