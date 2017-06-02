50e79bdc46e934b8d0c976ac024c0a21c9b436a3
Andrea Finch <aefinch@vt.edu>
2017-06-02 01:04:47 -0500

    board list shows up on click for adding pin to board

--- javascripts/controllers/PinViewCtrl.js
+++ javascripts/controllers/PinViewCtrl.js
@@ -1,6 +1,7 @@
 app.controller("PinViewCtrl", function($routeParams, $rootScope, $scope, PinFactory, BoardFactory){
 	$scope.selectedPin = {};
 	$scope.getBoardList = {}
+	$scope.pinIt= false;
 	PinFactory.viewSinglePin($routeParams.id)
 	.then((results) => {
 		$scope.selectedPin = results.data;
@@ -9,8 +10,17 @@ app.controller("PinViewCtrl", function($routeParams, $rootScope, $scope, PinFact
 		console.log("error in getSinglePin", error);
 	});
 
+	$scope.pinToBoard = () => {
+		$scope.pinIt= true;
+	}
+
 	BoardFactory.getBoardList($rootScope.user.uid)
 	.then((results) => {
 		$scope.userBoards = results;
+		console.log($scope.userBoards);
 	})
+
+	$scope.addToBoard = (boardId) => {
+		console.log(boardId);
+	}
 });
--- javascripts/factories/PinFactory.js
+++ javascripts/factories/PinFactory.js
@@ -2,17 +2,13 @@ app.factory("PinFactory", function($q, $http, FIREBASE_CONFIG){
 	let getPinList = (boardId) => {
 		let pinz = [];
 		return $q((resolve, reject) => {
-			console.log("in PinFactory", boardId);
 			$http.get(`${FIREBASE_CONFIG.databaseURL}/pins.json?orderBy="boardid"&equalTo="${boardId}"`)
 			.then((fbPins) => {
-				console.log("fbPins", fbPins);
 				let pinCollection = fbPins.data;
-				console.log("pinCollection", pinCollection);
 				Object.keys(pinCollection).forEach((key) => {
 					pinCollection[key].id = key;
 					pinz.push(pinCollection[key]);
 				});
-				console.log("pinz", pinz);
 				resolve(pinz);
 			}).catch((error) => {
 				reject(error);
@@ -25,7 +21,6 @@ app.factory("PinFactory", function($q, $http, FIREBASE_CONFIG){
 			$http.get(`${FIREBASE_CONFIG.databaseURL}/pins/${id}.json`)
 			.then((results) => {
 				// results.id = id
-				console.log("results in PinFactory", results);
 				resolve(results);
 			}).catch((error) => {
 				reject(error);
@@ -56,4 +51,4 @@ app.factory("PinFactory", function($q, $http, FIREBASE_CONFIG){
 	};
 
 	return {getPinList:getPinList, viewSinglePin:viewSinglePin, postNewPin:postNewPin, deletePin:deletePin};
-};
+});
