0821e8abba03f753eb757549beb3c3d808797c3e
Andrea Finch <aefinch@vt.edu>
2017-06-03 10:26:17 -0500

    grabbing boardid from pinIt function

--- javascripts/controllers/PinAddCtrl.js
+++ javascripts/controllers/PinAddCtrl.js
@@ -1,6 +1,6 @@
 app.controller("PinAddCtrl", function($rootScope, $location, $scope, PinFactory){
 	// if($location.path()===)
-	$scope.newPin.boardid = "global";
+	// $scope.newPin.boardid = "global";
 	$scope.addNewPin = () => {
 		PinFactory.postNewItem($scope.newPin).then(() => {
 			$scope.newPin = {};
