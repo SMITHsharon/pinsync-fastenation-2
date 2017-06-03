0645717ee72d636b334e35d1bfd91140e8b2d29c
Andrea Finch <aefinch@vt.edu>
2017-06-03 10:35:45 -0500

    adding an if to pinAddCtrl

--- javascripts/controllers/PinAddCtrl.js
+++ javascripts/controllers/PinAddCtrl.js
@@ -1,6 +1,9 @@
 app.controller("PinAddCtrl", function($rootScope, $location, $scope, PinFactory){
-	// if($location.path()===)
-	// $scope.newPin.boardid = "global";
+	if($location.path()==='/pin/new'){
+		$scope.newPin.boardid = "global";
+	} else {
+		$scope.newPin.boardid = boardId;
+	}
 	$scope.addNewPin = () => {
 		PinFactory.postNewItem($scope.newPin).then(() => {
 			$scope.newPin = {};
